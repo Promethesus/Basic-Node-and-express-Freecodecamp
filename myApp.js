e5c3a37b46bcb74648c3755cd76cfa4484ba204a
Promethesus <JeremyAndrews707@gmail.com>
2021-05-03 04:38:05 -0700

    updated pathing issue for webapp test1

--- myApp.js
+++ myApp.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
-absolutePath = __dirname + /public/index.html
 
 app.get("/", function(req, res) {
-    res.sendFile(absolutePath);
+    res.sendFile(__dirname, "/views/index.html");
   });
 console.log("Hello World")
 
