526664cc85342fb71c07ddf3157b2cfe69c67dee
Promethesus <JeremyAndrews707@gmail.com>
2021-05-03 05:03:31 -0700

    updated with app.use and express.static to render css to page

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ var app = express();
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
+
+app.use(__dirname + "/public", express.static())
+
 console.log("Hello World")
 
 
