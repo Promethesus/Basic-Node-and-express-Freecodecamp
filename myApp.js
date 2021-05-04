54b78d9bd4efe59a9b2f0efdb20b531eb4a46faa
Promethesus <JeremyAndrews707@gmail.com>
2021-05-04 04:30:39 -0700

    corrected tabs within myapp.js

--- myApp.js
+++ myApp.js
@@ -5,17 +5,16 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
-  if (process.env.MESSAGE_STYLE=="uppercase"){
-    app.get("/json", function(req, res) {
-    return res.json({ "message": "HELLO JSON" })
-    });
-    }
-    else {
-    app.get('/json', function(req, res) {
-     return res.json({ 'message': 'Hello json' })
-    });
-    
-    }
+if (process.env.MESSAGE_STYLE=="uppercase"){
+  app.get("/json", function(req, res) {
+  return res.json({ "message": "HELLO JSON" })
+  });
+  }
+  else {
+  app.get('/json', function(req, res) {
+    return res.json({ 'message': 'Hello json' })
+  });
+};
 
 app.use("/public", express.static(__dirname + "/public"));
 
