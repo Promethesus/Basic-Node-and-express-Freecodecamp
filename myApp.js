0cacc54d8d7dda9bd0a75190e55948a69277a57c
Promethesus <JeremyAndrews707@gmail.com>
2021-05-04 03:56:53 -0700

    added a /json endpoint to the myApp.js file

--- myApp.js
+++ myApp.js
@@ -5,9 +5,17 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json"
+  });
+});
+
 app.use("/public", express.static(__dirname + "/public"));
 
 console.log("Hello World")
+
+
 
 
 
