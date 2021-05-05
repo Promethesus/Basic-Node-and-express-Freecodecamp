1fb71e45e876f6c017d811dd07a0c1719da1a943
Promethesus <JeremyAndrews707@gmail.com>
2021-05-05 04:28:43 -0700

    final fix for myApp.js

--- myApp.js
+++ myApp.js
@@ -5,16 +5,12 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
-if (process.env.MESSAGE_STYLE=="uppercase"){
-  app.get("/json", function(req, res) {
-  return res.json({ "message": "HELLO JSON" })
-  });
-  }
-  else {
-  app.get('/json', function(req, res) {
-    return res.json({ 'message': 'Hello json' })
-  });
-  }
+const message = "Hello json";
+app.get("/json", 
+    (req, res) => res.json(
+        {"message": process.env.MESSAGE_STYLE === "uppercase" ? message.toUpperCase() : message
+    })
+);
 
 app.use("/public", express.static(__dirname + "/public"));
 
