a01c0bb2c241321b106dd1637ee84f599849f65d
Promethesus <JeremyAndrews707@gmail.com>
2021-05-05 05:18:49 -0700

    corrected middleware fucntuon to contain next() to pass FCC requirements

--- myApp.js
+++ myApp.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 function getTheTime(){
-  return new Date.toString()
+  return new Date.toString();
 }
 
-app.get('/now', function(req, res, next) {
+app.get("/now", function(req, res, next) {
   req.time = getTheTime();
-  next()
+  next();
 }, function(req, res) {
-  res.json({time: req.time});
+  res.json({ time: req.time });
 });
 
 app.use(function mWareFunction( req, res, next){
